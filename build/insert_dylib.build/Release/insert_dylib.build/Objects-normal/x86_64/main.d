cfa4f1aad261cefbc6265805ba91fcbce089e14a
1finx1 <finx.thorburn@gmail.com>
2025-03-26 19:17:28 +0000

    asdasdasdasd

---
+++ build/insert_dylib.build/Release/insert_dylib.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/Darwin.modulemap \
+  /Users/finthorburn/Documents/bypass/insert_dylib/insert_dylib/main.c \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/c_standard_library.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/DarwinBasic.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/MachO.modulemap
